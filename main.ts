18e0744444a718e7f2f080e91096aa68e5eeca69
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-19 20:00:04 +0000

    add husky and maqueen Extension - basic check to see if hand gestures recognised

--- main.ts
+++ main.ts
@@ -2,5 +2,14 @@ huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.OBJECTCLASSIFICATION)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-	
+    huskylens.request()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.SmallDiamond)
+    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Square)
+    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
